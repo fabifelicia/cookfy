e204537794ad6cd5680347663c47760cba3fe496
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2022-02-09 13:06:49 -0300

    add action modal image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 const URL = 'https://randomuser.me/api/1.3'
 
+const modal1 = document.querySelector('.images-section .modal')
+const modal2 = document.querySelector('.container .modal')
+const plate1 = document.querySelector('.food-plate-1')
+const plate2 = document.querySelector('.food-plate-2')
+
+
+plate1.addEventListener('mouseover', () => {
+    modal1.classList.add('active')
+})
+
+plate1.addEventListener('mouseout', () => {
+    modal1.classList.remove('active')
+})
+
+plate2.addEventListener('mouseover', () => {
+    modal2.classList.toggle('active')
+})
+
+plate2.addEventListener('mouseout', () => {
+    modal2.classList.toggle('active')
+})
+
 const loginUser = (name) => {
     const nameAvatar = document.querySelector('.name-avatar')    
     nameAvatar.innerText = name
@@ -10,6 +32,9 @@ const photo= (avatar) => {
     photoProfile.src = avatar
 }
 
+ 
+ 
+
 async function getUser() {
     const data = await fetch(URL)
     .then(response => response.json())
@@ -19,7 +44,6 @@ async function getUser() {
 
       return(data.results[0])
 }
-
 
 
 
