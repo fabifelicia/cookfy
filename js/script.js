3c1b967766c4ee5d6d81e1cef691c2bff0f0d5f5
fabifelicia <fabiana.menezes@dcomp.ufs.br>
2022-02-09 17:10:03 -0300

    add buttons events and modal events

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-const URL = 'https://randomuser.me/api/1.3'
 
+// Images events - modal
 const modal1 = document.querySelector('.images-section .modal')
 const modal2 = document.querySelector('.container .modal')
 const plate1 = document.querySelector('.food-plate-1')
 const plate2 = document.querySelector('.food-plate-2')
-
 
 plate1.addEventListener('mouseover', () => {
     modal1.classList.add('active')
@@ -22,6 +21,47 @@ plate2.addEventListener('mouseout', () => {
     modal2.classList.toggle('active')
 })
 
+
+//Buttons events
+
+const [btnCook, btnPatisserie, btnDrinks] = document.querySelectorAll('.food-classification-2 button')
+
+btnCook.addEventListener('mouseover', () => {
+    document.getElementById('cook').src = './assets/Cozinha_active.svg'
+    document.getElementById('cook-name').style.color = '#e82147'
+})
+
+btnCook.addEventListener('mouseout', () => {
+    document.getElementById('cook').src = './assets/Cozinha.svg'
+    document.getElementById('cook-name').style.color = '#a19b9b'
+})
+
+btnPatisserie.addEventListener('mouseover', () => {
+    document.getElementById('patisserie').src = './assets/Patisserie_active.svg'
+    document.getElementById('patisserie-name').style.color = '#e82147'
+})
+
+btnPatisserie.addEventListener('mouseout', () => {
+    document.getElementById('patisserie').src = './assets/Patisserie.svg'
+    document.getElementById('patisserie-name').style.color = '#a19b9b'
+})
+
+btnDrinks.addEventListener('mouseover', () => {
+    document.getElementById('drinks').src = './assets/Drinks_active.svg'
+    document.getElementById('drinks-name').style.color = '#e82147'
+})
+
+btnDrinks.addEventListener('mouseout', () => {
+    document.getElementById('drinks').src = './assets/Drinks.svg'
+    document.getElementById('drinks-name').style.color = '#a19b9b'
+})
+
+
+
+//api events
+
+const URL = 'https://randomuser.me/api/1.3'
+
 const loginUser = (name) => {
     const nameAvatar = document.querySelector('.name-avatar')    
     nameAvatar.innerText = name
@@ -31,9 +71,6 @@ const photo= (avatar) => {
     const photoProfile = document.querySelector('.avatar')
     photoProfile.src = avatar
 }
-
- 
- 
 
 async function getUser() {
     const data = await fetch(URL)
@@ -45,17 +82,12 @@ async function getUser() {
       return(data.results[0])
 }
 
-
-
 async function User() {
     const user = await getUser()
     const { gender, name, phone, email, picture }  = user 
     
     loginUser(name.first)
     photo(picture.thumbnail)    
-    
-    
-    console.log(gender, name.last, phone, email)
 }
    
 User()
